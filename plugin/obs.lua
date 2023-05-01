6fdb0198b92eab1128d862a11be445d83ba56b47
Daniel Singer <zigius@gmail.com>
2023-05-01 11:50:11 +0300

    Fix typo in command name

--- plugin/obs.lua
+++ plugin/obs.lua
@@ -1,5 +1,5 @@
 -- Register the command in your plugin file
-vim.api.nvim_create_user_command("Obisidian", function()
+vim.api.nvim_create_user_command("Obsidian", function()
   require"obs".open_in_obsidian()
 end, { nargs = 0 })
 -- vim.api.nvim_create_user_command("SummarizeArticle", SummarizeArticle, { nargs = 1})
